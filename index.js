0a68a0f5753d9e3b458114e6d57c403c192b8ecd
User Name <artur.it2914@gmail.com>
2020-06-19 17:12:40 +0300

    Add Increment/Decrement examples

--- index.js
+++ index.js
@@ -1,58 +1,7 @@
-const firstInput = prompt( 'Please input your first number value' );
-const firstNumber = Number( firstInput );
+let count = 0;
+console.log( count );
 
-if (!isNaN( firstNumber )) {
-
-  const secondInput = prompt( 'Please input your second number value' );
-  const secondNumber = Number( secondInput );
-  if (!isNaN( secondNumber )) {
-
-    const operationMark = prompt( 'Please choose your operation (*, /, +, -)' );
-    let operationFunc;
-    switch (operationMark) {
-      case '+': {
-        operationFunc = sum;
-      }
-        break;
-      case '-': {
-        operationFunc = subtraction;
-      }
-        break;
-      case '*': {
-        operationFunc = multiple;
-      }
-        break;
-      case '/': {
-        operationFunc = division;
-      }
-        break;
-      default:
-        alert( 'Unknown option' );
-    }
-
-    if (typeof operationFunc === 'function') {
-      alert( operationFunc( firstNumber, secondNumber ) );
-    }
-    /*
-     * YOUR CODE IS HERE!
-     * */
-
-  }
-
-}
-
-function sum(a, b) {
-  return a + b;
-}
-
-function multiple(a, b) {
-  return a * b;
-}
-
-function subtraction(a, b) {
-  return a - b;
-}
-
-function division(a, b) {
-  return a / b;
-}
+const firstIncrement = ++count;
+const firstDecrement = --count;
+const secondIncrement = count++;
+const secondDecrement = count--;
