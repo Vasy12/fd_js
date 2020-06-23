27fc45774691a0735e1edfd9428afd120717d1f9
User Name <artur.it2914@gmail.com>
2020-06-23 17:06:17 +0300

    Add cycle for examples

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-let tryCount = 0;
-let isPasswordCorrect;
+/**
+ *
+ * @param {number} base
+ * @param {number} power
+ * @return {number}
+ */
+function pow(base, power) {
 
-do {
-
-  isPasswordCorrect = prompt( 'Please input your password' ) === CORRECT_PASS;
-
-  if (isPasswordCorrect) {
-    break;
+  if (power < 0) {
+    return 1 / pow( base, Math.abs( power ) );
   }
 
-} while (++tryCount < TRY_LIMIT);
+  let result = base;
 
-alert( `Password is ${isPasswordCorrect
-                      ? 'correct'
-                      : 'incorrect'}.` );
+  for (let i = 1; i < power; ++i) {
+    result *= base;
+  }
+
+  return result;
+}
+
+console.log( 'pow( 2, 3 ) = ', pow( 2, 3 ) );// 8;
+console.log( 'pow( 14, 0 ) = ', pow( 14, 0 ) );// 1;
+console.log( 'pow( 14, 0 ) = ', pow( 2, -3 ) );// 1 / 8;
+
+/**
+ *
+ * @param {number} n
+ * @return {number}
+ */
+function fib(n) {
+  if (n < 3) {
+    return 1;
+  }
+
+  let result;
+  let prev1 = 1;
+  let prev2 = 1;
+
+  for (let i = 2; i < n; i++) {
+    result = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = result;
+  }
+
+  return result;
+}
+
+console.log( fib( 8 ) );//21
+console.log( fib( 11 ) );//89
