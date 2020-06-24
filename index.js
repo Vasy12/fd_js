aa79ee15d792c6ac597c03121fda6b1ef276be91
User Name <artur.it2914@gmail.com>
2020-06-24 15:36:21 +0300

    Add Array example

--- index.js
+++ index.js
@@ -1,50 +1,35 @@
-/**
- *
- * @param {number} base
- * @param {number} power
- * @return {number}
- */
-function pow(base, power) {
+function MyArray() {
 
-  if (power < 0) {
-    return 1 / pow( base, Math.abs( power ) );
-  }
+  this.length = 0;
 
-  let result = base;
+  this.push = function push(item) {
 
-  for (let i = 1; i < power; ++i) {
-    result *= base;
-  }
+    this[this.length++] = item;
 
-  return result;
+  };
+
 }
 
-console.log( 'pow( 2, 3 ) = ', pow( 2, 3 ) );// 8;
-console.log( 'pow( 14, 0 ) = ', pow( 14, 0 ) );// 1;
-console.log( 'pow( 14, 0 ) = ', pow( 2, -3 ) );// 1 / 8;
+const myArr1 = new MyArray();
 
-/**
- *
- * @param {number} n
- * @return {number}
- */
-function fib(n) {
-  if (n < 3) {
-    return 1;
-  }
 
-  let result;
-  let prev1 = 1;
-  let prev2 = 1;
 
-  for (let i = 2; i < n; i++) {
-    result = prev1 + prev2;
-    prev1 = prev2;
-    prev2 = result;
-  }
+const arr1 = [];
 
-  return result;
+
+for (let i = 0; i < 10; i++) {
+  myArr1.push( `Item '${i}'` );
+  arr1.push( `Item '${i}'` );
 }
 
-console.log( fib( 8 ) );//21
-console.log( fib( 11 ) );//89
+const myArr2 = new MyArray();
+
+
+
+const arr2 = [];
+
+
+for (let i = 0; i < 10; i++) {
+  myArr2.push( `Item '${i}'` );
+  arr2.push( `Item '${i}'` );
+}
